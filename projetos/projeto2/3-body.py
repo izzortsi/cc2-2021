27f0fe2682a703a8c3f09e8f407b043a13048587
Igor Strozzi <istrozzi@matematica.ufrj.br>
2021-05-07 10:50:31 -0300

    gg proj2

--- projetos/projeto2/3-body.py
+++ projetos/projeto2/3-body.py
@@ -16,22 +16,17 @@ def Fij(ri, rj):
 
 
 def F(t, y):
-    _y = np.reshape(y, (N_BODIES, DIMENSION * 2))
+    _y = np.reshape(y, (N_BODIES, DIMENSION, 2))
     out = np.zeros_like(_y)
-    print(_y)
-    for i in range(0, N_BODIES, 2):
-        for j in range(0, N_BODIES, 2):
+
+    for i, body_state_i in enumerate(_y):
+        for j, body_state_j in enumerate(_y):
             if i != j:
-                out[i] = _y[i + 1]
-                out[i + 1] += Fij(_y[i], _y[j])
-                print(_y[i], _y[j])
+                out[i][0] = _y[i][1]
+                out[i][1] += Fij(_y[i][0], _y[j][0])
     return out.flatten()
 
 
-# initial_state = np.array(["x11", "x12", "v11", "v12", "x21", "x22", "v21", "v22"])
-# initial_state = np.array(
-#    [0, 0.5, -0.2, -0.2, 0.2, 0.5, -0.2, 0.2, 0.34, 0.38, -0.4, 0.0]
-# )
 # %%
 # 0.322184765624991, 0.647989160156249
 
@@ -54,25 +49,98 @@ initial_state = np.array(
 
 # %%
 
-
-# %%
-F(1, initial_state)
 rk4 = RK4()
 h = 0.01
 t0 = 0
 tf = 20
 ts, ys = rk4.solve(F, t0, tf, initial_state, h)
 
+
 _ys = ys.reshape((int(tf / h), N_BODIES, 4))
-_ys
+# _ys
 orbit_1 = _ys[:, 0, :2]
 orbit_2 = _ys[:, 1, :2]
 orbit_3 = _ys[:, 2, :2]
-orbit_1
+# orbit_1
 # %%
+orbits = [orbit_1, orbit_2, orbit_3]
+fig = plt.figure(figsize=(12, 6))
+ax = fig.add_subplot()
 
-fig, ax = plt.subplots(figsize=(12, 6))
-ax.plot(orbit_1[:, 0], orbit_1[:, 1], "o-", markersize=0.5)
-ax.plot(orbit_2[:, 0], orbit_2[:, 1], "o-", markersize=0.5)
-ax.plot(orbit_3[:, 0], orbit_3[:, 1], "o-", markersize=0.5)
+ax.set_xlim((-5, 5))
+ax.set_ylim((-5, 5))
+# orbit_plot_1 = ax.plot(orbit_1[:, 0], orbit_1[:, 1], "o-", markersize=0.5)
+# orbit_plot_2 = ax.plot(orbit_2[:, 0], orbit_2[:, 1], "o-", markersize=0.5)
+# orbit_plot_3 = ax.plot(orbit_3[:, 0], orbit_3[:, 1], "o-", markersize=0.5)
+# ax.lines
+# mass1 = ax.scatter(orbit_1[0, 0], orbit_1[0, 1], c="red", s=15)
+mass1 = ax.plot(
+    orbit_1[0, 0],
+    orbit_1[0, 1],
+    c="red",
+    marker="o",
+    markersize=10,
+)
+trail1 = ax.plot(
+    orbit_1[0, 0],
+    orbit_1[0, 1],
+    "--",
+    c="red",
+    lw=0.4,
+    alpha=0.5,
+)
+
+# mass2 = ax.scatter(orbit_2[0, 0], orbit_2[0, 1], c="green", s=15)
+mass2 = ax.plot(
+    orbit_2[0, 0],
+    orbit_2[0, 1],
+    c="green",
+    marker="o",
+    markersize=10,
+)
+trail2 = ax.plot(
+    orbit_2[0, 0],
+    orbit_2[0, 1],
+    "--",
+    c="green",
+    lw=0.4,
+    alpha=0.5,
+)
+
+# mass3 = ax.scatter(orbit_3[0, 0], orbit_3[0, 1], c="blue", s=15)
+mass3 = ax.plot(
+    orbit_3[0, 0],
+    orbit_3[0, 1],
+    c="blue",
+    marker="o",
+    markersize=10,
+)
+trail3 = ax.plot(
+    orbit_3[0, 0],
+    orbit_3[0, 1],
+    "--",
+    c="blue",
+    lw=0.4,
+    alpha=0.5,
+)
+
 fig
+
+from matplotlib.animation import FuncAnimation
+
+
+def update(num, orbits, ax):
+
+    for i, orbit in enumerate(orbits):
+        mass = ax.lines[2 * i]
+        trail = ax.lines[2 * i + 1]
+        mass.set_data(orbit[num, 0], orbit[num, 1])
+        trail.set_data(orbit[:num, 0], orbit[:num, 1])
+
+
+num_frames = len(ts)
+animation = FuncAnimation(
+    fig, update, frames=num_frames, fargs=(orbits, ax), interval=12
+)
+
+animation.save("teste.mp4", dpi=240, bitrate=1500)
