8f8b5d06c053afaa66bb638bc626e0021db3b177
Igor Strozzi <istrozzi@matematica.ufrj.br>
2021-05-08 05:00:53 -0300

    finished classic kuramoto model

--- projetos/projeto2/bonus/kuramoto.py
+++ projetos/projeto2/bonus/kuramoto.py
@@ -3,13 +3,18 @@ import os
 import numpy as np
 import numpy.linalg as la
 import matplotlib.animation as animation
+import matplotlib as mpl
 from IPython.display import HTML
 from matplotlib import pyplot as plt
 from explicit_rk import ExplicitRungeKutta, integrators
 
 # %%
+# https://matplotlib.org/stable/gallery/images_contours_and_fields/interpolation_methods.html
+mpl.rcParams["image.interpolation"] = "none"
+# %%
+
 np.random.seed(0)
-n = 80
+n = 25
 N = n ** 2
 K = 1
 ω = np.random.rand(N) * 2 * np.pi
@@ -30,12 +35,11 @@ ts, θs = rk4.solve(F, 0, 120, θ, 1)
 NUM_TS = len(ts)
 θs = θs.reshape(NUM_TS, n, n)
 # %%
-
-fig, ax = plt.subplots(figsize=(n // 10, n // 10))
-ax.set_axis_off()
-fig
-ax.matshow(θs[0])
-fig
+def init_plot():
+    fig, ax = plt.subplots(figsize=(n // 10, n // 10))
+    ax.set_axis_off()
+    ax.imshow(θs[0])
+    return ax.images
 
 
 def update(num, θs, ax):
@@ -46,10 +50,12 @@ def update(num, θs, ax):
 anim = animation.FuncAnimation(
     fig,
     update,
+    init_func=init_plot,
     frames=NUM_TS,
     fargs=(θs, ax),
-    interval=15,
+    interval=5,
     blit=True,
+    repeat=True,
 )
 
-anim.save("bonus/kuramoto.mp4")
+anim.save("bonus/kuramoto.mp4", fps=6)
