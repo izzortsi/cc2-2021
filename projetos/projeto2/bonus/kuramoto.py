f59ac83722a3bf94232a122c55452d343b56a7b1
Igor Strozzi <istrozzi@matematica.ufrj.br>
2021-05-08 04:36:37 -0300

    bonus: kuramoto

---
+++ projetos/projeto2/bonus/kuramoto.py
@@ -0,0 +1,55 @@
+# %%
+import os
+import numpy as np
+import numpy.linalg as la
+import matplotlib.animation as animation
+from IPython.display import HTML
+from matplotlib import pyplot as plt
+from explicit_rk import ExplicitRungeKutta, integrators
+
+# %%
+np.random.seed(0)
+n = 80
+N = n ** 2
+K = 1
+ω = np.random.rand(N) * 2 * np.pi
+θ = np.random.rand(N) * 2 * np.pi
+
+
+def F(t, θ):
+    dθ = np.zeros_like(θ)
+    for i, θ_i in enumerate(θ):
+        dθ[i] = ω[i] + (K / N) * np.sum(np.sin(θ - θ_i))
+    return dθ
+
+
+rk4 = integrators["RK4"]()
+# %%
+
+ts, θs = rk4.solve(F, 0, 120, θ, 1)
+NUM_TS = len(ts)
+θs = θs.reshape(NUM_TS, n, n)
+# %%
+
+fig, ax = plt.subplots(figsize=(n // 10, n // 10))
+ax.set_axis_off()
+fig
+ax.matshow(θs[0])
+fig
+
+
+def update(num, θs, ax):
+    ax.images[0].set_data(θs[num])
+    return ax.images
+
+
+anim = animation.FuncAnimation(
+    fig,
+    update,
+    frames=NUM_TS,
+    fargs=(θs, ax),
+    interval=15,
+    blit=True,
+)
+
+anim.save("bonus/kuramoto.mp4")
