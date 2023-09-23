f9b59e289abe4d5285bf4b253c3270e7f4bb1b66
Igor Strozzi <igor.strozzi@gmail.com>
2023-09-23 13:03:43 -0300

    projeto 1 - 2023

--- projetos/projeto1/proj1-23.py
+++ projetos/projeto1/proj1-23.py
@@ -4,22 +4,13 @@ from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 import argparse
 
-# Step 0: Parse the arguments
-parser = argparse.ArgumentParser(
-    description="Solves the ODE system Du = Au for a vector of dependent variables u, given the matrix A and a vector of initial conditions u0"
-)
-parser.add_argument("-dim", "--dimension", type=int, help="The dimension of the matrix A and vector u0", default = 0)
-parser.add_argument("--seed", type=int, help="Random seed to be used if randomly sampling the coefficients of A", default = -1)
-args = parser.parse_args()
-
-DIM = args.dimension if args.dimension > 0 else np.random.randint(3, 6)
-SEED = args.seed if args.seed >= 0 else np.random.randint(0, 100)
-np.random.seed(SEED)
-
-# Step 1: Analytical solution
 def analytical_solution(A, u0, t):
     eigvals, eigvecs = np.linalg.eig(A)
-    
+    # print("evals: \n", eigvals)
+    # print("evecs: \n", eigvecs)
+    # print(np.linalg.matrix_rank(eigvecs))
+    # print(len(eigvals))
+
     # Check if the matrix is defective
     defective = np.linalg.matrix_rank(eigvecs) < len(eigvals)
     
@@ -30,43 +21,63 @@ def analytical_solution(A, u0, t):
         u = np.array([sp.linalg.expm(A * t_i) @ u0 for t_i in t]).T
         return u        
 
-# Step 2: Numerical solution
 def numerical_solution(A, u0, t):
     def du_dt(t, u): return A @ u
     sol = solve_ivp(du_dt, [t[0], t[-1]], u0, t_eval=t)
     return sol.y
+def main():
 
-# Define the matrix A and initial condition u0
-# A = np.array([[0, 1], [-1, 0]])
-# u0 = np.array([1, 0])
+    # Uniformly sample the coefficients of A and the coordinates of u0 within [-COEFS_RANGE, COEFS_RANGE]
 
-# Uniformly sample the coefficients of A and the coordinates of u0 within [-1, 1]
+    A = np.random.uniform(-COEFS_RANGE, COEFS_RANGE, (DIM, DIM))
+    u0 = np.random.uniform(-COEFS_RANGE, COEFS_RANGE, DIM)
 
-A = np.random.uniform(-1, 1, (DIM, DIM))
-u0 = np.random.uniform(-1, 1, DIM)
+    # Define the time points where solution is computed
+    t = np.arange(0, FINALTIME, TIMESTEP)
 
-# Define the time points where solution is computed
-t = np.linspace(0, 10, 100)
+    # Get the analytical and numerical solutions
+    u_analytical = analytical_solution(A, u0, t)
+    u_numerical = numerical_solution(A, u0, t)
 
-# Get the analytical and numerical solutions
-u_analytical = analytical_solution(A, u0, t)
-u_numerical = numerical_solution(A, u0, t)
+    fig, axs = plt.subplots(DIM + 1, 1, figsize=(8, 2 * DIM))
+    errors = []
+    for i in range(DIM):
+        error_i = np.abs(u_analytical[i] - u_numerical[i])
+        errors.append(error_i)
+        axs[i].plot(t, u_analytical[i], label=f'Analytical solution (u{i+1})')
+        axs[i].plot(t, u_numerical[i], '--', label=f'Numerical solution (u{i+1})')
+        axs[i].set_xlabel('Time')
+        axs[i].set_ylabel(f'u{i+1}')
+        axs[i].legend()
 
-# Step 3: Plot the solutions
-fig, axs = plt.subplots(DIM + 1, 1, figsize=(8, 2 * DIM))
-for i in range(DIM):
-    axs[i].plot(t, u_analytical[i], label=f'Analytical solution (u{i+1})')
-    axs[i].plot(t, u_numerical[i], '--', label=f'Numerical solution (u{i+1})')
-    axs[i].set_xlabel('Time')
-    axs[i].set_ylabel(f'u{i+1}')
-    axs[i].legend()
+        axs[-1].plot(t, error_i, label=f'solution (u{i+1})')
+        axs[-1].set_xlabel('Time')
+        axs[-1].set_ylabel('Error')
+        axs[-1].set_title('Errors between analytical and numerical solutions')
+        axs[-1].legend()
 
-# Step 4: Plot the error between the solutions
-error = np.linalg.norm(u_analytical - u_numerical, axis=0)
-axs[-1].plot(t, error)
-axs[-1].set_xlabel('Time')
-axs[-1].set_ylabel('Error')
-axs[-1].set_title('Error between analytical and numerical solutions')
+    plt.tight_layout()
+    plt.show()
 
-plt.tight_layout()
-plt.show()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Solves the ODE system Du = Au for a vector of dependent variables u, given the matrix A and a vector of initial conditions u0"
+    )
+    parser.add_argument("-dim", "--dimension", type=int, help="The dimension of the matrix A and vector u0", default = 0)
+    parser.add_argument("--seed", type=int, help="Random seed to be used if randomly sampling the coefficients of A", default = -1)
+    parser.add_argument("--time_step", type=float, help="", default = 1e-3)
+    parser.add_argument("--final_time", type=float, help="", default = 2.0)
+    parser.add_argument("--coefs_range", type=float, help="", default = 4.0)
+    args = parser.parse_args()
+
+    DIM = args.dimension if args.dimension > 0 else np.random.randint(3, 6)
+    SEED = args.seed if args.seed >= 0 else np.random.randint(0, 100)
+    TIMESTEP = args.time_step
+    FINALTIME = args.final_time
+    COEFS_RANGE = args.coefs_range
+    np.random.seed(SEED)
+
+    main()
